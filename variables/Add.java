97d9b8305a234c9131f140a2e3c8c07375773209
unknown <jumajosephat61@gmail.com>
2024-02-20 04:37:19 -0800

    Added Numbers in java

---
+++ variables/Add.java
@@ -0,0 +1,9 @@
+class Add{
+    public static void main(String args[]){
+        int a=100;
+        int b=100;
+        int c=a+b;
+        System.out.println("The Sum of "+ a + " and " + b + " is ");
+        System.out.println(c);
+    }
+}
