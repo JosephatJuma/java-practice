53a5274b66f68b25265fa9c3361d50c20b0ad1f1
unknown <jumajosephat61@gmail.com>
2024-02-22 14:07:01 -0800

    opp,cindtionalls

---
+++ oop/Demo.java
@@ -0,0 +1,15 @@
+class Calculator{
+    public int add(int a, int b){
+        System.out.println("Inside add method");
+        return a + b;
+    }
+}
+
+
+class Demo {
+public static void main(String[] a){
+    Calculator calc=new Calculator();
+    System.out.println(calc.add(10,20));
+}
+    
+}
