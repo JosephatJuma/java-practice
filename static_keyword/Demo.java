ef4d312e0fbb724515b6f10e07becc675cdd746d
unknown <jumajosephat61@gmail.com>
2024-02-23 11:29:42 -0800

    static key word

---
+++ static_keyword/Demo.java
@@ -0,0 +1,36 @@
+
+class Mobile {
+    String brand;
+    int price;
+    static String name;
+    static {
+        name = "Smart Phone";
+    }
+
+    /*
+     * the static block is executed only once and first
+     * 
+     * each object of the class is has the same value of the static instance
+     * variable
+     */
+
+    public void display() {
+        System.out.println(brand + " " + price + " " + name);
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Mobile m1 = new Mobile();
+        Mobile m2 = new Mobile();
+        // Mobile.name = "Smart Phone";
+        m1.brand = "Iphone";
+        m2.brand = "Samsung";
+        m1.price = 1000;
+        m2.price = 2000;
+        m1.display();
+        m2.display();
+
+    }
+
+}
