b97112e90a5208e1421b31ef73461e36fea0ae23
unknown <jumajosephat61@gmail.com>
2024-02-26 02:16:56 -0800

    single level inheritence

---
+++ inheritence/AdvancedCalc.java
@@ -0,0 +1,12 @@
+
+public class AdvancedCalc extends Calculator {
+
+    public int nulti(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ inheritence/Demo.java
@@ -0,0 +1,9 @@
+class Demo {
+    public static void main(String[] args) {
+        AdvancedCalc calc = new AdvancedCalc();
+        System.out.println(calc.add(10, 20));
+        System.out.println(calc.sub(20, 20));
+        System.out.println(calc.nulti(10, 20));
+        System.out.println(calc.div(20, 20));
+    }
+}
