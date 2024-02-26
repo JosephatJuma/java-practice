a0eb7f6c3e37d4c8f93e89ba5590a10c87abce74
unknown <jumajosephat61@gmail.com>
2024-02-26 02:54:55 -0800

    understood the conceprts of overiding and overloading well

--- oop/inheritence/AdvCalc.java
+++ oop/inheritence/AdvCalc.java
@@ -4,6 +4,22 @@ public class AdvCalc extends Calc {
 
     }
 
+    /*
+     * the add method is overloaded because it has 3 parameters diffrent from two
+     * parameters of add from the pareent class
+     */
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    /*
+     * this is method overriding because the behavior of the method is diffrent
+     */
+
+    public int sub(int a, int b) {
+        return (a - b) + 1;
+    }
+
     public int div(int a, int b) {
         return a / b;
     }
