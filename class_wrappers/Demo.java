f3e8c0e45f5cc44a3306f03a0b9a3d027e3d3001
unknown <jumajosephat61@gmail.com>
2024-02-27 23:50:54 -0800

    the concept of class wrappers

--- class_wrappers/Demo.java
+++ class_wrappers/Demo.java
@@ -22,5 +22,31 @@ public class Demo {
 
         System.out.println(num3);
 
+        /*
+         * Other wrapper classes
+         * - Boolean
+         * - Character
+         * - Byte
+         * - Short
+         * - Long
+         * - Float
+         * - Double
+         * - String
+         * - Enum
+         * - Date
+         * 
+         */
+
+        String str = "10";
+        Integer num4 = Integer.parseInt(str);
+        System.out.println(num4.intValue());
+
+        Double num5 = Double.parseDouble(str);
+        System.out.println(num5);
+
+        Character ch = str.charAt(0);
+        String str1 = ch.toString().concat("hello");
+        System.out.println(str1);
+
     }
 }
